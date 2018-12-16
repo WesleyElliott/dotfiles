f4db63904c34ea74fcebad5b6c1df585c1559fbc
Wesley Elliott <wesleyjasonelliott@gmail.com>
2018-12-16 15:37:51 +0200

    Improve copy_dotfiles to symlink
    Having two copies (the git repo, and the actual config) confuses
    editting, making it harder to keep track. By symlinking them, theres
    only one true copy

--- copy_dotfiles.sh
+++ copy_dotfiles.sh
@@ -2,15 +2,35 @@
 
 . ./color.sh
 
-DOTFILES_DIR=.dotfiles
+DOTFILES_DIR="$(pwd)/.dotfiles"
 
-# Copy general dotfiles
-echo -e "${GREEN}Copying dotfiles...${NOCOLOR}"
-cp $DOTFILES_DIR/.imwheelrc $DOTFILES_DIR/.vimrc $DOTFILES_DIR/.zshrc ~/
+# Deprecated in favour of linking dotfiles, but kept just in case
+copy_dotfiles() {
+    # Copy general dotfiles
+    echo -e "${GREEN}Copying dotfiles...${NOCOLOR}"
+    cp $DOTFILES_DIR/.imwheelrc $DOTFILES_DIR/.vimrc $DOTFILES_DIR/.zshrc ~/
+    
+    # Copy machine specific dotfiles
+    
+    if [ -e $DOTFILES_DIR/.zshrc-$HOSTNAME ]; then
+        echo -e "${GREEN}zshrc found for $HOSTNAME, copying..${NOCOLOR}"
+        cp $DOTFILES_DIR/.zshrc-$HOSTNAME ~/
+    fi
+}
 
-# Copy machine specific dotfiles
+link_dotfiles() {
+    # Link the general dotfiles
+    echo -e "${GREEN}Linking dotfiles...${NOCOLOR}"
+    ln -sf $DOTFILES_DIR/.imwheelrc ~/.imwheelrc
+    ln -sf $DOTFILES_DIR/.vimrc ~/.vimrc
+    ln -sf $DOTFILES_DIR/.zshrc ~/.zshrc
 
-if [ -e $DOTFILES_DIR/.zshrc-$HOSTNAME ]; then
-    echo -e "${GREEN}zshrc found for $HOSTNAME, copying..${NOCOLOR}"
-    cp $DOTFILES_DIR/.zshrc-$HOSTNAME ~/
-fi
+    # Link machine specific dotfiles
+    if [ -e $DOTFILES_DIR/.zshrc-$HOSTNAME ]; then
+        echo -e "${GREEN}zshrc found for $HOSTNAME, linking...${NOCOLOR}"
+        ln -sf $DOTFILES_DIR/.zshrc-$HOSTNAME ~/.zshrc-$HOSTNAME
+    fi
+}
+
+# Run the link
+link_dotfiles
