36260a95364e0d9d6dc2764c061985acb9d04c15
Wesley <wesleyjasonelliott@gmail.com>
2021-04-02 18:35:31 +0300

    Reset

--- .scripts/wesley-laptop/toggle_imwheel.sh
+++
@@ -1,8 +0,0 @@
-if pgrep imwheel > /dev/null
-then
-    echo "[timwheel] Running, disabing"
-    imwheel -q -k
-else
-    "[timwheel] Not running, starting"
-    imwheel -b "4 5"
-fi
--- color.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-TEXT_RESET=$(tput sgr0) # reset the foreground colour
-RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-YELLOW=$(tput setaf 3)
