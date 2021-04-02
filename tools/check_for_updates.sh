c671903641e47e3ec583a61b69162119e1d34594
Wesley <wesleyjasonelliott@gmail.com>
2021-04-02 23:00:44 +0300

    Make it fancier

--- tools/check_for_updates.sh
+++ tools/check_for_updates.sh
@@ -1,11 +1,24 @@
+#!/usr/bin/env zsh
+ 
 BRANCH=$( dotfiles rev-parse --abbrev-ref HEAD )
 RESULT=0
 
+RED=$(printf '\033[31m')
+GREEN=$(printf '\033[32m')
+YELLOW=$(printf '\033[33m')
+BLUE=$(printf '\033[34m')
+BOLD=$(printf '\033[1m')
+DIM=$(printf '\033[2m')
+UNDER=$(printf '\033[4m')
+RESET=$(printf '\033[m')
+
 function push_dotfiles {
+    printf "${BLUE}%s${RESET}\n" "Pushing..."
     dotfiles push origin $BRANCH --quiet
 }
 
 function pull_dotfiles {
+    printf "${BLUE}%s${RESET}\n" "Updating..."
     dotfiles pull origin $BRANCH --quiet
 }
 
@@ -22,9 +35,10 @@ function check_remote {
 
 check_remote
 RESULT=$?
+
 if [ $RESULT = 1 ]; then
     # We are ahead, so ask to push changes
-    echo -n "[dotfiles] Unpushed changes found, push to origin? [Y/n]\n"
+    printf "${BLUE}%s${RESET}\n" "Push changes to origin? [Y/n]"
     read -r -k option
     case "$option" in
         [yY$'\n']) push_dotfiles ;;
@@ -32,7 +46,7 @@ if [ $RESULT = 1 ]; then
     esac
 elif [ $RESULT = -1 ]; then
     # We are behind, so ask to pull and update
-    echo -n "[dotfiles] Would you like to update? [Y/n]\n"
+    printf "${BLUE}%s${RESET}\n" "[dotfiles] Would you like to update? [Y/n]"
     read -r -k option
     case "$option" in
         [yY$'\n']) pull_dotfiles ;;
@@ -40,6 +54,6 @@ elif [ $RESULT = -1 ]; then
     esac
 elif [ $RESULT = -2 ]; then
     # We have diverged, inform of possible conflics
-    echo -n "[dotfiles] Local and remote changes found, please deal with conflicts and update\n"
+    printf "${RED}%s${RESET}\n" "[dotfiles] Local and remote are out of sync. Fix conflicts..."
 fi
 
