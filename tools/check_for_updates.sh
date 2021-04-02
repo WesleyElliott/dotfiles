e79419a2dbf7fe5bb231702e547b00e9dde0ddbc
Wesley <wesleyjasonelliott@gmail.com>
2021-04-02 21:40:34 +0300

    Improve update checks
    Now handles if a push is needed, a pull is needed, or conflicts possible

--- tools/check_for_updates.sh
+++ tools/check_for_updates.sh
@@ -1,23 +1,45 @@
 
-function update_dotfiles {
+function get_current_branch {
+    return git rev-parse --abbrev-ref HEAD
+}
+
+function push_dotfiles {
+    local branch=$(get_current_branch)
+    dotfiles push origin $branch
+}
+
+function pull_dotfiles {
+    local branch=$(get_current_branch)
     dotfiles pull
 }
 
 function check_remote {
-    dotfiles update-index --quiet --refresh
-    local CHANGES=0
-    dotfiles diff-index --quiet --exit-code HEAD -- || CHANGES=1
+    local branch=$(get_current_branch)
+    source $HOME/.functions
+    dotfiles fetch
     
-    return CHANGES
+    return dotfiles_status $branch
 }
 
 check_remote
 if [ $? = 1 ]; then
-    # There are updates, ask to update
-    echo -n "[dotfiles] Would you like to update? [Y/n] "
-    read-r -k 1 option
+    # We are ahead, so ask to push changes
+    echo -n "[dotfiles] Unpushed changes found, push to origin? [Y/n]\n"
+    read -r -k option
     case "$option" in
-        [yY$'\n']) update_dotfiles ;;
-        [nN]) echo "Not updating." ;;
+        [yY$'\n']) push_dotfiles ;;
+        [nN]) echo "Not pushing.\n" ;;
     esac
+elif [ $? = -1 ]; then
+    # We are behind, so ask to pull and update
+    echo -n "[dotfiles] Would you like to update? [Y/n]\n"
+    read -r -k option
+    case "$option" in
+        [yY$'\n']) pull_dotfiles ;;
+        [nN]) echo "Not updating.\n" ;;
+    esac
+elif [ $? = -2 ]; then
+    # We have diverged, inform of possible conflics
+    echo -n "[dotfiles] Local and remote changes found, please deal with conflicts and update\n"
 fi
+
