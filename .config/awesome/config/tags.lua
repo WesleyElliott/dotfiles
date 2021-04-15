0edede0459050fe488493344f274ed67a73a26e8
Wesley <wesleyjasonelliott@gmail.com>
2021-04-15 22:21:26 +0300

    Fix rofi background issues
    Remove gaps for dev workspace

--- .config/awesome/config/tags.lua
+++ .config/awesome/config/tags.lua
@@ -8,5 +8,12 @@ awful.screen.connect_for_each_screen(function(screen)
         layout = awful.layout.suit.float
     })
 
-    awful.tag({"\u{f268}", "\u{f120}", "\u{f121}"}, screen, awful.layout.suit.tile)
+    awful.tag({"\u{f268}", "\u{f120}"}, screen, awful.layout.suit.tile)
+
+    awful.tag.add("\u{f121}", {
+        screen = screen,
+        layout = awful.layout.suit.float,
+        gal_single_client = false,
+        gap = 0
+    })
 end)
